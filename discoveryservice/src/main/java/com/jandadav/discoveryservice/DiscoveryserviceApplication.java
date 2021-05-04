ec77484bff9ad27493088b2c0ff9cd2a67590898
jandadav <janda.david@gmail.com>
2021-05-04 13:47:15 +0200

    register api

--- discoveryservice/src/main/java/com/jandadav/discoveryservice/DiscoveryserviceApplication.java
+++ discoveryservice/src/main/java/com/jandadav/discoveryservice/DiscoveryserviceApplication.java
@@ -1,13 +1,26 @@
 package com.jandadav.discoveryservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryserviceApplication.class, args);
+    }
+
+    @EventListener
+    public void onStart(ApplicationReadyEvent event) {
+        System.out.println("Eureka Ready");
     }
 
 }
