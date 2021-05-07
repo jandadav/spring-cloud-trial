d1cfa64e1f228361a17a89e01f43bb898b80094f
jandadav <janda.david@gmail.com>
2021-05-07 16:20:46 +0200

    CircuitBreaker filters

---
+++ gateway/src/main/java/com/jandadav/gateway/CircuitBreakreCustomizer.java
@@ -0,0 +1,22 @@
+package com.jandadav.gateway;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+
+public class CircuitBreakreCustomizer {
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
+                .build());
+    }
+
+}
